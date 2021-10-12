4ab56950e8ba9fbce9c3fc2776c43200063994c4
Edwin <zizo_edwin@hotmail.com>
2021-10-12 11:07:00 +0800

    Change TaskList Methods and Added to Parser, Add logger for TaskList

--- src/main/java/task/TaskList.java
+++ src/main/java/task/TaskList.java
@@ -2,11 +2,15 @@ package task;
 
 import command.Parser;
 import command.Ui;
+
+import java.lang.reflect.Array;
 import java.time.DateTimeException;
 import java.time.LocalDateTime;
 import java.time.Period;
 import java.util.ArrayList;
 import java.util.Comparator;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 
 public class TaskList {
@@ -14,8 +18,10 @@ public class TaskList {
     private static final int DAYS_IN_A_WEEK = 7;
     private static final int DAYS_IN_A_MONTH = 31;
     private static final int DAYS_IN_A_YEAR = 366;
+    private static final Logger logger = Logger.getLogger(TaskList.class.getName());
     protected ArrayList<Task> taskList;
     protected int taskCount;
+
 
 
     public TaskList() {
@@ -24,31 +30,38 @@ public class TaskList {
 
     //Getters
     public int getTaskCount() {
+        logger.log(Level.INFO, "Get task count");
         return taskCount;
     }
 
     public ArrayList<Task> getTaskList() {
+        logger.log(Level.INFO, "Get task list");
         return taskList;
     }
 
     public int size() {
+        logger.log(Level.INFO, "Get size of task list");
         return this.taskList.size();
     }
 
     //Setters
     public void setTaskList(ArrayList<Task> taskList) {
+        logger.log(Level.INFO, "Set task list");
         this.taskList = taskList;
     }
 
     public void setTaskCount(int taskCount) {
+        logger.log(Level.INFO, "Set task count");
         this.taskCount = taskCount;
     }
 
     public Task get(int index) {
+        logger.log(Level.INFO, "Get task");
         return this.taskList.get(index);
     }
 
     public void delete(Task task) {
+        logger.log(Level.INFO, "Delete task");
         taskList.remove(task);
         this.taskCount = taskList.size();
     }
@@ -77,6 +90,7 @@ public class TaskList {
      * @param userInput task description input by user
      */
     public void addTask(String module, String userInput) throws DateTimeException {
+        logger.log(Level.INFO, "Add task");
         String date = getDate(userInput);
         if (date.isBlank()) {
             Ui.missingDate();
@@ -105,24 +119,21 @@ public class TaskList {
         return 0;
     };
 
-    private boolean isWeekly(Task t) {
-        Task refTask = taskList.get(0);
+    private boolean isWeekly(Task refTask, Task t) {
         LocalDateTime refDay = refTask.deadline;
         Period p = Period.between(refDay.toLocalDate(), t.deadline.toLocalDate()).normalized();
         int day = p.getYears() * 366 + p.getMonths() * 31 + p.getDays();
         return day <= DAYS_IN_A_WEEK;
     }
 
-    private boolean isMonthly(Task t) {
-        Task refTask = taskList.get(0);
+    private boolean isMonthly(Task refTask, Task t) {
         LocalDateTime refDay = refTask.deadline;
         Period p = Period.between(refDay.toLocalDate(), t.deadline.toLocalDate()).normalized();
         int day = p.getYears() * 366 + p.getMonths() * 31 + p.getDays();
         return day <= DAYS_IN_A_MONTH;
     }
 
-    private boolean isYearly(Task t) {
-        Task refTask = taskList.get(0);
+    private boolean isYearly(Task refTask, Task t) {
         LocalDateTime refDay = refTask.deadline;
         Period p = Period.between(refDay.toLocalDate(), t.deadline.toLocalDate()).normalized();
         int day = p.getYears() * 366 + p.getMonths() * 31 + p.getDays();
@@ -130,37 +141,30 @@ public class TaskList {
     }
 
     public void showAllWeekly(String module) {
-        taskList.sort(sortByDate);
-        int index = 1;
+        ArrayList<Task> tmp = new ArrayList<>(taskList);
+        tmp.sort(sortByDate);
         Ui.printWeeklyTaskList(module);
-        for (Task task : taskList) {
-            if (task != null && isWeekly(task)) {
-                System.out.print(index + ".");
-                System.out.println(task);
-                index++;
-            }
-        }
+        printWeekly(tmp.get(0));
     }
 
     public void showAllMonthly(String module) {
-        taskList.sort(sortByDate);
-        int index = 1;
+        ArrayList<Task> tmp = new ArrayList<>(taskList);
+        tmp.sort(sortByDate);
         Ui.printMonthlyTaskList(module);
-        for (Task task : taskList) {
-            if (task != null && isMonthly(task)) {
-                System.out.print(index + ".");
-                System.out.println(task);
-                index++;
-            }
-        }
+        printMonthly(tmp.get(0));
     }
 
     public void showAllYearly(String module) {
-        taskList.sort(sortByDate);
-        int index = 1;
+        ArrayList<Task> tmp = new ArrayList<>(taskList);
+        tmp.sort(sortByDate);
         Ui.printYearlyTaskList(module);
+        printYearly(tmp.get(0));
+    }
+
+    private void printWeekly(Task tmp) {
+        int index = 1;
         for (Task task : taskList) {
-            if (task != null && isYearly(task)) {
+            if (task != null && isWeekly(tmp, task)) {
                 System.out.print(index + ".");
                 System.out.println(task);
                 index++;
@@ -168,11 +172,31 @@ public class TaskList {
         }
     }
 
-    public void printTaskList(String module) {
-        taskList.sort(sortByDate);
-        taskList.sort(sortByStatus);
+    private void printMonthly(Task tmp) {
         int index = 1;
+        for (Task task : taskList) {
+            if (task != null && isMonthly(tmp, task)) {
+                System.out.print(index + ".");
+                System.out.println(task);
+                index++;
+            }
+        }
+    }
+
+    private void printYearly(Task tmp) {
+        int index = 1;
+        for (Task task : taskList) {
+            if (task != null && isYearly(tmp, task)) {
+                System.out.print(index + ".");
+                System.out.println(task);
+                index++;
+            }
+        }
+    }
+
+    public void printTasks(String module) {
         Ui.printTaskList(module);
+        int index = 1;
         for (Task task : taskList) {
             if (task != null) {
                 System.out.print(index + ".");
@@ -180,6 +204,14 @@ public class TaskList {
                 index++;
             }
         }
+    }
+
+    public void sortTaskListByDate(String module) {
+        taskList.sort(sortByDate);
+    }
+
+    public void sortTaskListByStatus(String module) {
+        taskList.sort(sortByStatus);
     }
 
     @Override

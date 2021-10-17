3a184a628354deffebddae4ebd3e4ebffe11425f
Dmitri <dmitriyam.rex1738@gmail.com>
2021-10-17 17:48:52 +0800

    Gradable tasks implemented

--- src/test/java/module/ModuleTest.java
+++ src/test/java/module/ModuleTest.java
@@ -64,32 +64,15 @@ class ModuleTest {
         m.addTask("do tutorial /by 12/11/2021 2000");
         m.addGradableTask("Assignment /by 12/12/2021 1600 /w 30");
         m.addGradableTask("Finals /by 15/12/2021 1600 /w 50");
-        String exp = "Module name: cs2113T\n" +
-                "CREDITS: 4\n" +
-                "--------------------------- \n" +
-                "SCHEDULE: \n" +
-                "1.\n" +
-                "Day: WED\n" +
-                "Start Time: 0800\n" +
-                "Location: E1-01\n" +
-                "Comments: tut\n" +
-                "2.\n" +
-                "Day: TUE\n" +
-                "Start Time: 0800\n" +
-                "Location: E1-01\n" +
-                "Comments: lect\n" +
-                "3.\n" +
-                "Day: MON\n" +
-                "Start Time: 1000\n" +
-                "Location: E1-01\n" +
-                "Comments: lect\n" +
-                "--------------------------- \n" +
-                "GRADE: A+\n" +
-                "TASKS: [[ ] read by: 11 Nov 2021 07:00 PM, [ ] do tutorial by: 12 Nov 2021 08:00 PM]\n" +
-                "BREAKDOWN: \n1.\n" +
-                "Assignment by: 12 Dec 2021 04:00 PM Weightage 30% [ ] \n" +
-                "2.\n" +
-                "Finals by: 15 Dec 2021 04:00 PM Weightage 50% [ ] \n";
+        String exp = "Module name: cs2113T\n"
+                + "CREDITS: 4\n" + "--------------------------- \n" + "SCHEDULE: \n" + "1.\n"
+                + "Day: WED\n" + "Start Time: 0800\n" + "Location: E1-01\n" + "Comments: tut\n" + "2.\n"
+                + "Day: TUE\n" + "Start Time: 0800\n" + "Location: E1-01\n" + "Comments: lect\n" + "3.\n"
+                + "Day: MON\n" + "Start Time: 1000\n" + "Location: E1-01\n" + "Comments: lect\n"
+                + "--------------------------- \n" + "GRADE: A+\n" +
+                "TASKS: [[ ] read by: 11 Nov 2021 07:00 PM, [ ] do tutorial by: 12 Nov 2021 08:00 PM]\n"
+                + "BREAKDOWN: \n1.\n" + "Assignment by: 12 Dec 2021 04:00 PM Weightage 30% [ ] \n"
+                + "2.\n" + "Finals by: 15 Dec 2021 04:00 PM Weightage 50% [ ] \n";
         assertEquals(exp, m.toString());
     }
 }

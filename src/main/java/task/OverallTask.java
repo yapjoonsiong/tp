55760845e723ea1bfe5dbc74228095b0a24381d2
naijie2108 <yeatnaijie@gmail.com>
2021-10-23 15:05:24 +0800

    Add javadoc comments
    - For Storage and OverallTask

--- src/main/java/task/OverallTask.java
+++ src/main/java/task/OverallTask.java
@@ -2,11 +2,21 @@ package task;
 
 import java.util.Comparator;
 
+/**
+ * Task class used for listing out all available tasks in a semester.
+ * Inherits from task class but adds a moduleName, weightage and gradable attribute.
+ */
 public class OverallTask extends Task {
     private final String moduleName;
     private final boolean gradable;
     private int weightage;
 
+    /**
+     * Constructor for overall task which takes in a Task object.
+     *
+     * @param task Task object used to construct the OverallTask object
+     * @param moduleName Name of the module of the task
+     */
     public OverallTask(Task task, String moduleName) {
         super(task.description, task.date);
         this.isDone = task.isDone;
@@ -14,6 +24,12 @@ public class OverallTask extends Task {
         this.gradable = false;
     }
 
+    /**
+     * Constructor for overall task which takes in a GradableTask object.
+     *
+     * @param task GradableTask object used to construct the OverallTask object
+     * @param moduleName Name of the module of the task
+     */
     public OverallTask(GradableTask task, String moduleName) {
         super(task.description, task.date);
         this.isDone = task.isDone;
@@ -22,8 +38,14 @@ public class OverallTask extends Task {
         this.weightage = task.weightage;
     }
 
+    /**
+     * Comparator used to compare deadline between OverallTask objects.
+     */
     public static Comparator<OverallTask> dateComparator = Comparator.comparing(t -> t.deadline);
 
+    /**
+     * Comparator used to compare "done" status between OverallTask objects.
+     */
     public static Comparator<OverallTask> statusComparator = (task1, task2) -> {
         if (task1.isDone && !task2.isDone) {
             return -1;

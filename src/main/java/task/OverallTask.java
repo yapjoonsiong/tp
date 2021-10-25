aa19eaf62b6ed89d9fb616e8a949bfdaa74cdc77
LAPTOP-61K9T4RL\Nai Jie <yeatnaijie@gmail.com>
2021-10-25 22:10:45 +0800

    Add class diagram to DG

--- src/main/java/task/OverallTask.java
+++ src/main/java/task/OverallTask.java
@@ -8,7 +8,7 @@ import java.util.Comparator;
  */
 public class OverallTask extends Task {
     private final String moduleName;
-    private final boolean gradable;
+    private final boolean isGradable;
     private int weightage;
 
     /**
@@ -21,7 +21,7 @@ public class OverallTask extends Task {
         super(task.description, task.date);
         this.isDone = task.isDone;
         this.moduleName = moduleName;
-        this.gradable = false;
+        this.isGradable = false;
         this.isLate = task.isLate;
     }
 
@@ -35,12 +35,12 @@ public class OverallTask extends Task {
         super(task.description, task.date);
         this.isDone = task.isDone;
         this.moduleName = moduleName;
-        this.gradable = true;
+        this.isGradable = true;
         this.weightage = task.weightage;
     }
 
     public boolean isGradable() {
-        return gradable;
+        return isGradable;
     }
 
     /**
@@ -62,11 +62,11 @@ public class OverallTask extends Task {
     };
 
     private String getGradableString() {
-        return gradable ? "[G]" : "[ ]";
+        return isGradable ? "[G]" : "[ ]";
     }
 
     private String getWeightageString() {
-        return gradable ? "[Weightage: " + this.weightage + "%]" : "";
+        return isGradable ? "[Weightage: " + this.weightage + "%]" : "";
     }
 
     private String getModuleNameString() {

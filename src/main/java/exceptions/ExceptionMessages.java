4a5ea447af84ee034a2882c7657f214876a6ee0b
naijie2108 <yeatnaijie@gmail.com>
2021-11-06 02:01:17 +0800

    Fix checkstyle issues

--- src/main/java/exceptions/ExceptionMessages.java
+++ src/main/java/exceptions/ExceptionMessages.java
@@ -1,7 +1,7 @@
 package exceptions;
 
 /**
- * Class used to store exception messages used in NoCapExceptions
+ * Class used to store exception messages used in NoCapExceptions.
  */
 public class ExceptionMessages {
     public static final String EXCEPTION_CREATE_DIRECTORY_FAIL = "Error creating directory";

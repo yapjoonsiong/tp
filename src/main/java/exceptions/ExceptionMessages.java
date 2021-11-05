1c26139325f0675cd7722eed1b1142a8c378ac8f
naijie2108 <yeatnaijie@gmail.com>
2021-11-06 01:59:57 +0800

    Add documentation to public methods

--- src/main/java/exceptions/ExceptionMessages.java
+++ src/main/java/exceptions/ExceptionMessages.java
@@ -1,7 +1,9 @@
 package exceptions;
 
+/**
+ * Class used to store exception messages used in NoCapExceptions
+ */
 public class ExceptionMessages {
     public static final String EXCEPTION_CREATE_DIRECTORY_FAIL = "Error creating directory";
     public static final String EXCEPTION_CREATE_FILE_FAIL = "Error creating save file";
-    public static final String EXCEPTION_SEMESTER_INDEX_OUT_OF_BOUNDS = "Semester index has to be between 1 to 10";
 }
